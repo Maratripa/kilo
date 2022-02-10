a19779150e841b75f943a63bea667cff4dc135fb
Titin <maratripa@gmail.com>
2022-02-10 15:45:35 -0300

    Added cursor functionality

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@
 /*** data ***/
 
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -169,8 +170,11 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H", 3); // Set cursor to 1, 1
 
     editorDrawRows(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
     
-    abAppend(&ab, "\x1b[H", 3); // Set cursor to 1, 1
     abAppend(&ab, "\x1b[?25h", 6); // Show cursor
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -178,6 +182,23 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
+
+void editorMoveCursor(char key) {
+    switch (key) {
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
+}
 
 void editorProcessKeypress() {
     char c = editorReadKey();
@@ -188,15 +209,23 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+
+        case 'w':
+        case 's':
+        case 'a':
+        case 'd':
+            editorMoveCursor(c);
+            break;
     }
 }
 
 /*** init ***/
 
 void initEditor() {
-    if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
-        die("getWindowSize");
-    }
+    E.cx = 0;
+    E.cy = 0;
+
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main() {
